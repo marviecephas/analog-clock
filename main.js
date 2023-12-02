3040f8c71c0c3d79df19e9a90e86f0127a9ad2b6
marvellous <120428022+marviecephas@users.noreply.github.com>
2023-12-02 05:30:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+let second = 0;
+let minute = 0;
+let hour = 0;
+let time = new Date();
+
+setInterval (
+    function() {
+        time = new Date()
+        minute = time.getMinutes() * 6;
+        second = time.getSeconds() * 6;
+        hour = time.getHours() * 30;
+        document.querySelector('.minute_hand_wrapper').style.transform = "rotate(" + minute + "deg)";
+        document.querySelector('.second_hand_wrapper').style.transform = "rotate(" + second + "deg)";
+        document.querySelector('.hour_hand_wrapper').style.transform = "rotate(" + hour + "deg)";
+    },1000
+)
+
+console.log(Date())
 
